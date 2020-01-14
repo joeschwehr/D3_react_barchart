eb59d037cd88d943426dae7391e541e0c62ea5ef
Joe <joeschwehr@gmail.com>
2020-01-14 12:15:12 -0800

    Updates

---
+++ precache-manifest.7c77ef4e84def19139235c4e4f4c8b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1542e9b2cd96c1183e0811b14e19fae7",
+    "url": "/D3_react_barchart/index.html"
+  },
+  {
+    "revision": "2faf432c506c7e65bf09",
+    "url": "/D3_react_barchart/static/css/main.9a9d7085.chunk.css"
+  },
+  {
+    "revision": "79fad865b7e10bbfbef0",
+    "url": "/D3_react_barchart/static/js/2.47d2fd6a.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/D3_react_barchart/static/js/2.47d2fd6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2faf432c506c7e65bf09",
+    "url": "/D3_react_barchart/static/js/main.7f0b9288.chunk.js"
+  },
+  {
+    "revision": "025ecc1884e89e3cc4a3",
+    "url": "/D3_react_barchart/static/js/runtime-main.d651d96d.js"
+  }
+]);
